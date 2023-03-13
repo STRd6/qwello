fbb80377afa37b1fe3efbb4ab828d8166f326bff
Daniel Moore <daniel@builder.io>
2023-03-13 14:08:05 -0700

    empty image

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
-import { QwikLogo } from '../icons/qwik';
-import styles from './header.css?inline';
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { QwikLogo } from "../icons/qwik";
+import styles from "./header.css?inline";
 
 export default component$(() => {
   useStylesScoped$(styles);
@@ -8,7 +8,6 @@ export default component$(() => {
   return (
     <header>
       I'm the header component
-      <img id="emptyImage" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" width="0" height="0"/>
       <div class="debug" />
     </header>
   );
